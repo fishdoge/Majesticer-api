6b0e88e1aaf10921cc6e97b80b1a6c76198e4b7f
Aaron <ru19wu06@gmail.com>
2024-12-23 15:16:55 +0800

    add : call data on sui

--- node-sdk/data/getAssetValue.ts
+++ node-sdk/data/getAssetValue.ts
@@ -24,16 +24,17 @@ async function getAllCoinObject() {
 }
 
 async function getAllOenObject() {
+    
     try {
       const currentEpoch = await client.getOwnedObjects({owner:'0xff9a1e70f9287d6681cbc25dd4061fc478af2fee7b6bf16f6361b6a600644aa9'});
       currentEpoch.data.forEach((element) => {
         console.log(element)
       });
 
-      //console.log(currentEpoch.length)
+      
     } catch (error) {
       console.error('Error fetching current epoch:', error);
     }
 }
 
-getAllOenObject()
+getAllCoinObject()
