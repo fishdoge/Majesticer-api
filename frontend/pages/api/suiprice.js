6aa4998e2ff1894822804ce3eb2d02b0fa91f2d0
aaron <ru19wu06@gmail.com>
2024-12-26 10:59:18 +0800

    updateAPI

---
+++ frontend/pages/api/suiprice.js
@@ -0,0 +1,13 @@
+
+
+export default async function handler(req, res) {
+
+    // res.setHeader('Access-Control-Allow-Origin', 'https://www.pudding.one/');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
+    // res.setHeader('Access-Control-Allow-Origin', 'https://fortune-tell-web.vercel.app/');
+
+
+
+    res.send(enterMessage.toString());
+
+}
