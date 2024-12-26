a1f0ac7ac168759b51e9c4cbc060e58fe86dc205
Aaron <ru19wu06@gmail.com>
2024-12-26 19:26:26 +0800

    update:test module

---
+++ apitest/src/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const array = ['sui_price','deep_price']
+
+async function fetchUsers() {
+
+    for(let a= 0;a<1200;a++){
+        array.forEach(async(index)=>{
+            console.log(index);
+            try {
+                const response = await axios.get(`https://www.majesticer.xyz/api/${index}`);
+                console.log('GET Response:', response.data);
+                return response.data;
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        })
+    }
+ 
+}
+
+fetchUsers()
