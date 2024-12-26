b6043bb7bb96cbd8c782852cd7fd76fdf9f9f92d
Aaron <ru19wu06@gmail.com>
2024-12-26 15:58:04 +0800

    fix

--- frontend/pages/api/suiprice.js
+++ frontend/pages/api/suiprice.js
@@ -1,10 +1,10 @@
-import { suiPriceInNumber } from '../deepbook.ts'
+import { suiPrice } from '../deepbook.ts'
 
 export default async function handler(req, res) {
 
     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
-    const suiPrice = await suiPriceInNumber()
-    console.log(suiPrice)
-    res.send(suiPrice);
+    const suiPriceInString = await suiPrice()
+    console.log(suiPriceInString)
+    res.send(suiPriceInString);
 
 }
---
+++ frontend/pages/api/suiprice_number.js
@@ -0,0 +1,10 @@
+import { suiPriceInNumber } from '../deepbook.ts'
+
+export default async function handler(req, res) {
+
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
+    const suiPrice = await suiPriceInNumber()
+    console.log(suiPrice)
+    res.send(suiPrice);
+
+}
