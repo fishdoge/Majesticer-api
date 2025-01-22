f9a46dd5f5bae0bda828a3da4785f6deb8952e38
LI-YONG-QI <a2369028@gmail.com>
2025-01-22 13:32:37 +0800

    feat: min & avg price

--- frontend/components/data-chart.tsx
+++ frontend/components/data-chart.tsx
@@ -8,13 +8,16 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 
-import data from "../price.json";
-import maxData from "../max-price.json";
+import data from "../data/price.json";
+import maxPrice from "../data/max-price.json";
+import minPrice from "../data/min-price.json";
+import avgPrice from "../data/avg-price.json";
 
 export default function DataChart() {
   return (
     <section className="py-16 px-4 md:px-6 lg:px-8 hidden lg:block ">
       <div className="container mx-auto max-w-7xl ">
+        {/* sui price per hour */}
         <Card className="bg-gray-800/50 border-gray-700 ">
           <CardHeader>
             <CardTitle className="text-white capitalize">
@@ -56,19 +59,12 @@ export default function DataChart() {
                     strokeWidth={2}
                     stroke="var(--color-transactions)"
                   />
-                  {/* <Line
-                    type="monotone"
-                    dataKey="users"
-                    strokeWidth={2}
-                    activeDot={{ r: 6 }}
-                    stroke="var(--color-users)"
-                  /> */}
                 </LineChart>
               </ResponsiveContainer>
             </ChartContainer>
           </CardContent>
         </Card>
-
+        {/* sui max price per day */}
         <Card className="bg-gray-800/50 border-gray-700 ">
           <CardHeader>
             <CardTitle className="text-white capitalize">
@@ -90,7 +86,7 @@ export default function DataChart() {
               className="h-[500px] w-full"
             >
               <ResponsiveContainer width="100%" height="100%">
-                <LineChart data={maxData}>
+                <LineChart data={maxPrice}>
                   <XAxis
                     dataKey="timestamp"
                     stroke="#888888"
@@ -110,13 +106,100 @@ export default function DataChart() {
                     strokeWidth={2}
                     stroke="var(--color-users)"
                   />
-                  {/* <Line
+                </LineChart>
+              </ResponsiveContainer>
+            </ChartContainer>
+          </CardContent>
+        </Card>
+        {/* sui min price per day */}
+        <Card className="bg-gray-800/50 border-gray-700 ">
+          <CardHeader>
+            <CardTitle className="text-white capitalize">
+              sui min price per day
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer
+              config={{
+                transactions: {
+                  label: "api request : ",
+                  color: "hsl(var(--chart-1))",
+                },
+                users: {
+                  label: "Active Users",
+                  color: "hsl(var(--chart-2))",
+                },
+              }}
+              className="h-[500px] w-full"
+            >
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart data={minPrice}>
+                  <XAxis
+                    dataKey="timestamp"
+                    stroke="#888888"
+                    fontSize={12}
+                    tickFormatter={(value) => value.split("T00:00:00+08:00")[0]} // 去除時間部分
+                  />
+                  <YAxis
+                    stroke="#888888"
+                    fontSize={12}
+                    domain={[4, 5.5]} //! 手動調整範圍
+                    tickCount={10}
+                  />
+                  <ChartTooltip content={<ChartTooltipContent />} />
+                  <Line
                     type="monotone"
-                    dataKey="users"
+                    dataKey="value"
                     strokeWidth={2}
-                    activeDot={{ r: 6 }}
                     stroke="var(--color-users)"
-                  /> */}
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            </ChartContainer>
+          </CardContent>
+        </Card>
+        {/* sui avg price per day */}
+        <Card className="bg-gray-800/50 border-gray-700 ">
+          <CardHeader>
+            <CardTitle className="text-white capitalize">
+              sui avg price
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer
+              config={{
+                transactions: {
+                  label: "api request : ",
+                  color: "hsl(var(--chart-1))",
+                },
+                users: {
+                  label: "Active Users",
+                  color: "hsl(var(--chart-2))",
+                },
+              }}
+              className="h-[500px] w-full"
+            >
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart data={avgPrice}>
+                  <XAxis
+                    dataKey="timestamp"
+                    stroke="#888888"
+                    fontSize={12}
+                    tickFormatter={(value) => value.split("T00:00:00+08:00")[0]} // 去除時間部分
+                  />
+                  <YAxis
+                    stroke="#888888"
+                    fontSize={12}
+                    domain={[4, 5.5]} //! 手動調整範圍
+                    tickCount={10}
+                  />
+                  <ChartTooltip content={<ChartTooltipContent />} />
+                  <Line
+                    type="monotone"
+                    dataKey="value"
+                    strokeWidth={2}
+                    stroke="var(--color-users)"
+                  />
                 </LineChart>
               </ResponsiveContainer>
             </ChartContainer>
